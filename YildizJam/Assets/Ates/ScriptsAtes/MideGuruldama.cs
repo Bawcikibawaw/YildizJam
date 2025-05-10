8cddd7dc8f5f3f385f8aa24b186a0dee84301150
Mehmet Ali Atik <alimehmetatik@gmail.com>
2025-05-10 19:47:49 +0300

    guruldama ses

--- YildizJam/Assets/Ates/ScriptsAtes/MideGuruldama.cs
+++ YildizJam/Assets/Ates/ScriptsAtes/MideGuruldama.cs
@@ -9,9 +9,12 @@ namespace Ates.ScriptsAtes
         private BokDash bokdash;
         private BokJump bokjump;
         public bool loopStarted;
+        public AudioSource audioSource;
+        public AudioClip guruldamaSound;
 
         void Start()
         {
+            audioSource = GameObject.FindGameObjectWithTag("Sound").GetComponent<AudioSource>();
             hasGuruldama = false;
             bokdash = GameObject.FindGameObjectWithTag("Player").GetComponent<BokDash>();
             bokjump = GameObject.FindGameObjectWithTag("Player").GetComponent<BokJump>();
@@ -19,6 +22,7 @@ namespace Ates.ScriptsAtes
     
         void Update()
         {
+            Debug.Log(audioSource);
             if (!hasGuruldama && !loopStarted)
             {
                 loopStarted = true;
@@ -29,6 +33,7 @@ namespace Ates.ScriptsAtes
         private IEnumerator Guruldama()
         {
             yield return new WaitForSeconds(5f);
+            audioSource.PlayOneShot(guruldamaSound);
             hasGuruldama = true;
             bokdash.enabled = false;
             bokjump.enabled = false;
